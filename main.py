838ebf5731ccd4258c963092318cee2987703c77
Michael Davis <mcdavis1982@gmail.com>
2025-07-05 12:10:34 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 import streamlit as st
 import yfinance as yf
 import pandas as pd
-import pandas_ta as ta
+from ta.momentum import RSIIndicator
+from ta.trend import SMAIndicator
 import matplotlib.pyplot as plt
 import io
 import contextlib
